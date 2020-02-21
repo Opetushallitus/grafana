c83d1e7dd36d20deb4d88035b801a16c98f96813
Juho Majasaari <juho.majasaari@gofore.com>
2020-02-21 09:07:48 +0200

    Add error handling to Pingdom proxy

--- grafana_proxy/proxy.js
+++ grafana_proxy/proxy.js
@@ -4,7 +4,7 @@ const zlib = require("zlib");
 
 const app = express();
 
-app.get('/health', (req, res) => res.send('This is fine.'))
+app.get("/health", (req, res) => res.send("This is fine."));
 
 app.use(
   "/api/2.1/checks",
@@ -18,36 +18,41 @@ app.use(
         originalBody = Buffer.concat([originalBody, data]);
       });
       proxyRes.on("end", function() {
-        const bodyString = zlib.gunzipSync(originalBody).toString("utf-8");
-        const newBody = JSON.parse(bodyString);
+        try {
+          const bodyString = zlib.gunzipSync(originalBody).toString("utf-8");
+          const newBody = JSON.parse(bodyString);
 
-        checksFiltered = newBody.checks.filter(
-          check => check.status != "paused"
-        );
-        checksFilteredCount = checksFiltered.length;
-        failedCount = checksFiltered.filter(object => object.status !== "up")
-          .length;
+          checksFiltered = newBody.checks.filter(
+            check => check.status != "paused"
+          );
+          checksFilteredCount = checksFiltered.length;
+          failedCount = checksFiltered.filter(object => object.status !== "up")
+            .length;
 
-        newBody.extra = {
-          check_count: checksFilteredCount,
-          failed_check_count: failedCount
-        };
+          newBody.extra = {
+            check_count: checksFilteredCount,
+            failed_check_count: failedCount
+          };
 
-        res.set({
-          "content-type": "text/json; charset=utf-8",
-          "content-encoding": "gzip"
-        });
-        res.write(zlib.gzipSync(JSON.stringify(newBody)));
-        res.end();
+          res.set({
+            "content-type": "text/json; charset=utf-8",
+            "content-encoding": "gzip"
+          });
+          res.write(zlib.gzipSync(JSON.stringify(newBody)));
+          res.end();
+        } catch (err) {
+          res.writeHead(500, {
+            "Content-Type": "text/plain"
+          });
+          res.end(err.toString());
+        }
       });
     },
     onError(proxyErr, req, res) {
       res.writeHead(500, {
         "Content-Type": "text/plain"
       });
-      res.end(
-        "Error"
-      );
+      res.end("Error");
     }
   })
 );
