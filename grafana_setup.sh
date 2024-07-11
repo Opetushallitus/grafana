aff8574bf50d03e667f5b8798751250afefc12d8
Tuomas Paulin <tuomas.paulin@polarsquad.com>
2024-07-11 13:42:06 +0300

    fix sed

--- grafana_setup.sh
+++ grafana_setup.sh
@@ -4,7 +4,7 @@ echo "Setting admin password..."
 curl -s -X PUT -H "Content-Type: application/json" -d "{ \"oldPassword\": \"admin\", \"newPassword\":\"$GRAFANA_ADMIN_PASSWORD\", \"confirmNew\":\"$GRAFANA_ADMIN_PASSWORD\" }" http://admin:admin@localhost:3000/api/user/password
 
 echo "Editing data sources..."
-sed -i -e "s/PROMETHEUS_URL/${PROMETHEUS_URL}/g" /etc/grafana/datasource/*
+sed -i -e "s~PROMETHEUS_URL~${PROMETHEUS_URL}~g" /etc/grafana/datasource/*
 for i in `ls /etc/grafana/datasource/*.json`; do
   echo "Creating $i..."
   curl -s -H "Content-Type: application/json" --request POST --data @$i \
